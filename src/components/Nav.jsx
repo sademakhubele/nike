faeb7029efeb3335a299e38e67d2d3ea3482391d
sademakhubele <157542678+sademakhubele@users.noreply.github.com>
2024-09-21 16:28:56 +0200

    Initial commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,42 @@
+import { headerLogo } from '../assets/images'
+import { hamburger } from '../assets/icons'
+import { navLinks } from '../constants'
+
+const Nav = () => {
+  return (
+    <header className="padding-x absolute py-8 x-10 w-full">
+        <nav className='flex justify-between items-center max-container'>
+            <a href="/">
+              <img 
+              src={headerLogo} 
+              alt="Logo" 
+              width={130}
+              height={29}
+              />
+            </a>
+            <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+                {navLinks.map((item) => (
+                    <li key={item.label}>
+                        <a 
+                        href={item.href} 
+                        className='text-lg font-montserrat leading-normal text-slate-gray'>
+                           {item.label}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+            <div className='flex gap-2 text-lg leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24'>
+          <a href='/'>Sign in</a>
+          <span>/</span>
+          <a href='/' className='text-slate-gray'>
+          Explore</a>
+        </div>
+        <div className='hidden max-lg:block'>
+          <img src={hamburger} alt='hamburger icon' width={25} height={25} />
+        </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Nav
